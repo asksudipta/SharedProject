a2da8aeb78aff2219545526357ca47c4f8f20c4a
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-04-27 14:01:11 +0200

    Initial commit

---
+++ src/main/java/lexicon/se/App.java
@@ -0,0 +1,16 @@
+package lexicon.se;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static void myName(){
+        System.out.println("May name is Sudipta");
+    }
+}
